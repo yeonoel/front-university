6303d7c5b9a3d49b7e84021d0487be546ee6637d
yeonoel <yeopevroguinoel@gmail.com>
2025-11-25 19:38:22 +0000

    update env variable

--- src/index.js
+++ src/index.js
@@ -3,32 +3,32 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Accueil from './pages/Accueil';
 import GlobalStyle from './utils/styles/GlobalStyle';
-import {AuthProvider, ThemeProvider} from './utils/context';
+import { AuthProvider, ThemeProvider } from './utils/context';
 import Details from './pages/Details';
 import './index.css';
 import Inscription from './pages/inscription';
-import MainLayout from './Layout/MainLayout';
+import MainLayout from './layout/MainLayout';
 import Connexion from './pages/connexion';
 import LandingPage from './pages/LandingPage';
 import AjouterAvis from './pages/AjouterAvis';
-import PublicRoute from './Layout/PublicRoute';
+import PublicRoute from './layout/PublicRoute';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <Router>
+  <Router>
     <ThemeProvider>
       <AuthProvider>
         <GlobalStyle />
         <Routes>
-          <Route path="/" element={ <LandingPage />} />
-          <Route path="/ajouteravis" element={ <AjouterAvis />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/ajouteravis" element={<AjouterAvis />} />
           <Route path="/Accueil" element={<MainLayout><Accueil /></MainLayout>} />
           <Route path="/Accueil/search" element={<MainLayout><Accueil /></MainLayout>} />
           <Route path="/detail-university/:id" element={<MainLayout><Details /></MainLayout>} />
           <Route path="/inscription" element={<PublicRoute> <Inscription /></PublicRoute>} />
           <Route path="/connexion" element={<PublicRoute> <Connexion /></PublicRoute>} />
         </Routes>
-        </AuthProvider>
+      </AuthProvider>
     </ThemeProvider>
   </Router>
 );
