d9d4a4f90138ef572d0ba99b1b1cfb7a1dd7f8b8
yeonoel <yeopevroguinoel@gmail.com>
2025-11-25 20:02:09 +0000

    git ignore

--- src/layout/mainLayout.jsx
+++ src/layout/mainLayout.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-function MainLayout({children}) {
+function MainLayout({ children }) {
     return (
         <div>
             <Header />
